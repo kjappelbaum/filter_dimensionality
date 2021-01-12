9356ff24d40b8e51acd9ac991676d0624dea8eb9
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-01-12 17:49:46 +0100

    feat: push the latest version for ben

--- filter_metal_channels/screening.py
+++ filter_metal_channels/screening.py
@@ -12,13 +12,8 @@ from six.moves import zip
 from structure_comp.checker import Checker
 from tqdm.autonotebook import tqdm
 
-from .filter_metal_channels import (
-    get_channel_dimensionality,
-    get_channels,
-    get_metal_metadata,
-    get_metallic_substructure,
-    get_structure_properties,
-)
+from .filter_metal_channels import (get_channel_dimensionality, get_channels, get_metal_metadata,
+                                    get_metallic_substructure, get_structure_properties)
 from .utils import get_bibliometric_information, get_structure_list, isnotebook
 
 logger = logging.getLogger()
@@ -31,8 +26,8 @@ class ScreenMetalChannel:
         self,
         structure_list: list,
         njobs: int = 2,
-        feature_mode: str = "cheap",
-        other_metals: list = ["Al", "In", "Ga"],
+        feature_mode: str = 'cheap',
+        other_metals: list = ['Al', 'In', 'Ga'],
     ):
         self.structure_list = structure_list
         self.njobs = njobs
@@ -40,14 +35,10 @@ class ScreenMetalChannel:
         self.other_metals = other_metals
 
         if isnotebook():
-            logger.warning(
-                "the code is untested in notebook enviornments and is likely to not work there"
-            )
+            logger.warning('the code is untested in notebook enviornments and is likely to not work there')
 
     @classmethod
-    def from_folders(
-        cls, folder_1: str, extension: str = "cif", njobs=2, feature_mode: str = "cheap"
-    ):
+    def from_folders(cls, folder_1: str, extension: str = 'cif', njobs=2, feature_mode: str = 'cheap'):
         """Constructor method for a ScreenMetalChannel object"""
         sl_1 = get_structure_list(folder_1, extension)
         return cls(sl_1, njobs=njobs, feature_mode=feature_mode)
@@ -55,8 +46,8 @@ class ScreenMetalChannel:
     @staticmethod
     def _check_structure(  # pylint:disable=dangerous-default-value
         structure_path: str,
-        feature_mode: str = "cheap",
-        other_metals: list = ["Al", "In", "Ga"],
+        feature_mode: str = 'cheap',
+        other_metals: list = ['Al', 'In', 'Ga'],
     ) -> int:
         """
         Gets number of channels for one cif file.
@@ -72,9 +63,9 @@ class ScreenMetalChannel:
         """
         try:
             s = Structure.from_file(structure_path)
-            s1_metall1, s1_nonmetall = get_metallic_substructure(
-                s, return_non_metal_structure=True, other_metals=other_metals
-            )
+            s1_metall1, s1_nonmetall = get_metallic_substructure(s,
+                                                                 return_non_metal_structure=True,
+                                                                 other_metals=other_metals)
 
             num_channels = get_channels(s1_metall1, s1_nonmetall)
             if len(num_channels) > 0:
@@ -82,19 +73,19 @@ class ScreenMetalChannel:
                 metadata = get_metal_metadata(s1_metall1, num_channels[0][0])
                 structure_features = get_structure_properties(s, mode=feature_mode)
 
-                structure_features["num_channels"] = len(num_channels)
-                structure_features["channel_indices"] = num_channels
+                structure_features['num_channels'] = len(num_channels)
+                structure_features['channel_indices'] = num_channels
                 structure_features.update(metadata)
                 problems = Checker.flag_potential_problems(
                     structure_path,
                     clashing_threshold=0.1,
                     bond_threshold=2.0,
-                    unbound_mode="naive",
-                    hydrogen_mode="CH",
+                    unbound_mode='naive',
+                    hydrogen_mode='CH',
                 )
                 structure_features.update(problems)
                 planes = get_channel_dimensionality(num_channels, s1_metall1)
-                structure_features["planes"] = planes
+                structure_features['planes'] = planes
                 # ToDo: make this optional, as this only will be of use if the filenames are CSD refcodes
                 # bib = get_bibliometric_information(structure_path)
                 # structure_features.update(bib)
@@ -116,21 +107,19 @@ class ScreenMetalChannel:
 
             i = 0
             for structure, structure_features in tqdm(
-                list(
-                    zip(
+                    list(zip(
                         self.structure_list,
                         executor.map(partial_structure_check, self.structure_list),
-                    )
-                ),
-                total=len(self.structure_list),
+                    )),
+                    total=len(self.structure_list),
             ):
                 if structure_features:
-                    structure_features["name"] = structure
+                    structure_features['name'] = structure
                     channel_list.append(structure_features)
                     i += 1
 
                 if i % 100 == 0:
                     df = pd.DataFrame(channel_list)
-                    df.to_csv("backup_{}.csv".format(i))
+                    df.to_csv('backup_{}.csv'.format(i))
 
         return pd.DataFrame(channel_list)
--- filter_metal_channels/utils.py
+++ filter_metal_channels/utils.py
@@ -8,18 +8,17 @@ import re
 from glob import glob
 from pathlib import Path
 
+from ccdc import io
 from crossref.restful import Works
 from pyscopus import Scopus
 
-from ccdc import io
-
-scopus = Scopus(os.environ["SCOPUS_API_KEY"])
+scopus = Scopus(os.environ['SCOPUS_API_KEY'])
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def get_structure_list(directory: str, extension: str = "cif") -> list:
+def get_structure_list(directory: str, extension: str = 'cif') -> list:
     """
 
     Args:
@@ -29,17 +28,15 @@ def get_structure_list(directory: str, extension: str = "cif") -> list:
     Returns:
 
     """
-    logger.info("getting structure list")
+    logger.info('getting structure list')
     if extension:
-        structure_list = glob(os.path.join(directory, "".join(["*.", extension])))
+        structure_list = glob(os.path.join(directory, ''.join(['*.', extension])))
     else:
-        structure_list = glob(os.path.join(directory, "*"))
+        structure_list = glob(os.path.join(directory, '*'))
     return structure_list
 
 
-def get_bibliometric_information(
-    cif_path,
-):  # pylint:disable=too-many-locals, too-many-statements, too-many-branches
+def get_bibliometric_information(cif_path,):  # pylint:disable=too-many-locals, too-many-statements, too-many-branches
     """
     Assumes that the filename is the CSD-key.
     Assumes you are in the EPFL VPN.
@@ -77,7 +74,7 @@ def get_bibliometric_information(
     formula = None
 
     try:  # pylint:disable=too-many-nested-blocks
-        csd_reader = io.EntryReader("CSD")
+        csd_reader = io.EntryReader('CSD')
 
         reader = csd_reader.entry(stem)
         disorder_csd = reader.disorder_details
@@ -89,77 +86,75 @@ def get_bibliometric_information(
         chemical_name = reader.chemical_name
         formula = reader.formula
 
-        uv_regex_list = ["uv", "uv-vis", "vis"]
-        uv_regex = re.compile("|".join(uv_regex_list), re.IGNORECASE)
+        uv_regex_list = ['uv', 'uv-vis', 'vis']
+        uv_regex = re.compile('|'.join(uv_regex_list), re.IGNORECASE)
         uv_regex_result = False
 
-        photo_regex_list = ["photo", "absorp", "light", "lumin"]
-        photo_regex = re.compile("|".join(photo_regex_list), re.IGNORECASE)
+        photo_regex_list = ['photo', 'absorp', 'light', 'lumin']
+        photo_regex = re.compile('|'.join(photo_regex_list), re.IGNORECASE)
         photo_regex_result = False
 
-        electronic_regex_list = ["electronic", "conduc"]
-        electronic_regex = re.compile("|".join(electronic_regex_list), re.IGNORECASE)
+        electronic_regex_list = ['electronic', 'conduc']
+        electronic_regex = re.compile('|'.join(electronic_regex_list), re.IGNORECASE)
         electronic_regex_result = False
 
         if doi_paper:
-            logger.info("found DOI %s", doi_paper)
+            logger.info('found DOI %s', doi_paper)
             works = Works()
             query_res = works.doi(doi_paper)
             if query_res:
 
-                if "title" in query_res.keys():
-                    if len(query_res["title"]) > 0:
-                        title = query_res["title"][-1]
+                if 'title' in query_res.keys():
+                    if len(query_res['title']) > 0:
+                        title = query_res['title'][-1]
                     else:
-                        title = query_res["title"]
+                        title = query_res['title']
 
-                if "is-referenced-by-count" in query_res.keys():
-                    citations = query_res["is-referenced-by-count"]
+                if 'is-referenced-by-count' in query_res.keys():
+                    citations = query_res['is-referenced-by-count']
 
-                if "link" in query_res.keys():
-                    if len(query_res["link"]) > 0:
-                        url = query_res["link"][0]["URL"]
+                if 'link' in query_res.keys():
+                    if len(query_res['link']) > 0:
+                        url = query_res['link'][0]['URL']
                     else:
-                        url = query_res["link"]
+                        url = query_res['link']
 
-                if "abstract" in query_res.keys():
-                    abstract = query_res["abstract"]
+                if 'abstract' in query_res.keys():
+                    abstract = query_res['abstract']
 
                 funding_sublis = []
-                if "funder" in query_res.keys():
-                    for f in query_res["funder"]:
-                        funding_sublis.append(f["name"])
+                if 'funder' in query_res.keys():
+                    for f in query_res['funder']:
+                        funding_sublis.append(f['name'])
 
                     funding = funding_sublis
                 else:
                     funding = None
 
-                if "page" in query_res.keys():
-                    pages = query_res["page"]
+                if 'page' in query_res.keys():
+                    pages = query_res['page']
 
-                if "container-title" in query_res.keys():
-                    if len(query_res["container-title"]) > 0:
-                        journal = query_res["container-title"][0]
+                if 'container-title' in query_res.keys():
+                    if len(query_res['container-title']) > 0:
+                        journal = query_res['container-title'][0]
                     else:
-                        journal = query_res["container-title"]
+                        journal = query_res['container-title']
 
-                if "author" in query_res.keys():
+                if 'author' in query_res.keys():
                     author_sublist = []
-                    for a in query_res["author"]:
-                        author_sublist.append(a["family"])
+                    for a in query_res['author']:
+                        author_sublist.append(a['family'])
                     authors = author_sublist
 
                     affiliation_sublist = []
-                    for a in query_res["author"]:
-                        affiliation_sublist.append(a["affiliation"])
+                    for a in query_res['author']:
+                        affiliation_sublist.append(a['affiliation'])
 
                 if abstract is None:
                     search_result = scopus.search(doi_paper)
                     if len(search_result) > 0:
                         try:
-                            abstract = scopus.retrieve_abstract(
-                                search_result["scopus_id"].values[0]
-                            )["abstract"]
+                            abstract = scopus.retrieve_abstract(search_result['scopus_id'].values[0])['abstract']
                         except Exception:
                             abstract = None
 
@@ -177,57 +172,57 @@ def get_bibliometric_information(
                 photo_regex_result = True
 
     except Exception:
-        logger.info("Could not retrieve CSD info")
+        logger.info('Could not retrieve CSD info')
 
         result_dict = {
-            "deposition_number": deposition_number,
-            "title": title,
-            "csd_abbrv": stem,
-            "citations": citations,
-            "url": url,
-            "abstract": abstract,
-            "funding": funding,
-            "doi_ccsd": doi_ccsd,
-            "doi_paper": doi_paper,
-            "formula": formula,
-            "pages": pages,
-            "journal": journal,
-            "affilations": affilations,
-            "year": year,
-            "disorder_csd": disorder_csd,
-            "authors": authors,
-            "remarks": csd_remarks,
-            "csd_has_disorder": csd_has_disorder,
-            "chemical_name": chemical_name,
-            "uv_regex_result": uv_regex_result,
-            "photo_regex_result": photo_regex_result,
-            "electronic_regex_result": electronic_regex_result,
+            'deposition_number': deposition_number,
+            'title': title,
+            'csd_abbrv': stem,
+            'citations': citations,
+            'url': url,
+            'abstract': abstract,
+            'funding': funding,
+            'doi_ccsd': doi_ccsd,
+            'doi_paper': doi_paper,
+            'formula': formula,
+            'pages': pages,
+            'journal': journal,
+            'affilations': affilations,
+            'year': year,
+            'disorder_csd': disorder_csd,
+            'authors': authors,
+            'remarks': csd_remarks,
+            'csd_has_disorder': csd_has_disorder,
+            'chemical_name': chemical_name,
+            'uv_regex_result': uv_regex_result,
+            'photo_regex_result': photo_regex_result,
+            'electronic_regex_result': electronic_regex_result,
         }
 
     else:
         result_dict = {
-            "deposition_number": deposition_number,
-            "title": title,
-            "csd_abbrv": stem,
-            "citations": citations,
-            "url": url,
-            "abstract": abstract,
-            "funding": funding,
-            "doi_ccsd": doi_ccsd,
-            "doi_paper": doi_paper,
-            "formula": formula,
-            "pages": pages,
-            "journal": journal,
-            "affilations": affilations,
-            "year": year,
-            "disorder_csd": disorder_csd,
-            "authors": authors,
-            "remarks": csd_remarks,
-            "csd_has_disorder": csd_has_disorder,
-            "chemical_name": chemical_name,
-            "uv_regex_result": uv_regex_result,
-            "photo_regex_result": photo_regex_result,
-            "electronic_regex_result": electronic_regex_result,
+            'deposition_number': deposition_number,
+            'title': title,
+            'csd_abbrv': stem,
+            'citations': citations,
+            'url': url,
+            'abstract': abstract,
+            'funding': funding,
+            'doi_ccsd': doi_ccsd,
+            'doi_paper': doi_paper,
+            'formula': formula,
+            'pages': pages,
+            'journal': journal,
+            'affilations': affilations,
+            'year': year,
+            'disorder_csd': disorder_csd,
+            'authors': authors,
+            'remarks': csd_remarks,
+            'csd_has_disorder': csd_has_disorder,
+            'chemical_name': chemical_name,
+            'uv_regex_result': uv_regex_result,
+            'photo_regex_result': photo_regex_result,
+            'electronic_regex_result': electronic_regex_result,
         }
 
     return result_dict
@@ -236,9 +231,9 @@ def get_bibliometric_information(
 def isnotebook():
     try:
         shell = get_ipython().__class__.__name__
-        if shell == "ZMQInteractiveShell":
+        if shell == 'ZMQInteractiveShell':
             return True  # Jupyter notebook or qtconsole
-        elif shell == "TerminalInteractiveShell":
+        elif shell == 'TerminalInteractiveShell':
             return False  # Terminal   IPython
         return False  # Other type (?)
     except NameError:
